63b08774c1ed175a97a992bc9c23943a4e00256f
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-12 20:15:07 -0500

    bugfix

--- poobuntu-clean.sh
+++ poobuntu-clean.sh
@@ -10,7 +10,7 @@ apt-fast purge --autoremove -y `cat poobuntu-dpkg.list`
 apt-fast purge --autoremove -y apt-fast
 add-apt-repository -r ppa:apt-fast/stable
 apt purge --autoremove -y software-properties-common
-apt purge --autoremove -y apt-utils
+apt purge --autoremove -y dialog apt-utils
 apt clean
 rm -rf /var/lib/apt/lists/*
 rm -v poobuntu-dpkg.list /etc/profile.d/makeflags.sh
