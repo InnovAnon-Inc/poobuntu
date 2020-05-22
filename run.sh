796c46ae5ef94398a22d80986b03adff63555c22
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-05-21 23:04:33 -0500

    auto update

--- run.sh
+++ run.sh
@@ -16,13 +16,17 @@ dockerd &
 
 if [ -z ${1+x} ] ; then
 	CMD='docker build -t poobuntu .'
+	PUSH='docker push innovanon/poobuntu:latest'
 else
 	#CMD="docker build -t poobuntu-$1 --build-arg DOCKER_TAG=$1 ."
 	CMD="docker build -t poobuntu-$1 --build-arg VERSION=$1 ."
+	PUSH="docker push innovanon/poobuntu-$1:latest"
 fi
 
 sudo             -- \
 nice -n -20      -- \
 sudo -u `whoami` -- \
 $CMD
+
+$PUSH || :
 
