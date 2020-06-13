3180b7f4726530553a31f37d31a7da6185224d2e
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-13 03:32:57 -0500

    auto commit by ./run.sh

--- tlrz/tlrz.sh
+++ tlrz/tlrz.sh
@@ -1,7 +1,26 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-T="$(mktemp)"
-tar -O "$T" -cf -
-lrzip -z -U --outfile - "$T"
+D=0
+while getopts "d" arg ; do
+  case $arg in
+    d)
+      D=1
+      ;;
+    *) # invalid option
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+(( ! $# ))
+
+if (( ! "$D" )) ; then
+  T="$(mktemp)"
+  trap "rm -rf $T" 0
+  cat > "$T"
+  lrzip -z -U -q --outfile - -- "$T"
+else
+  lrunzip -f -q --outfile -
+fi
 
