838be5b4d03268c043dbed4de7b4e3026972c973
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-06-12 21:03:36 -0500

    auto commit by ./run.sh

--- acng/entrypoint.sh
+++ acng/entrypoint.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/env bash
 set -euxo pipefail
 
-exec start-stop-daemon --start --exec $(which cron) -- \
-	cron -n -L "${LOGLEVEL:-4}"
+#exec start-stop-daemon --start --exec $(which cron) -- \
+#	cron -n -L "${LOGLEVEL:-4}"
+cron -n -L "${LOGLEVEL:-4}"
 
 /sbin/entrypoint.sh
 
